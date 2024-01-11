37ac96272dadf97f7650906386e9cb8caa25cd24
Saga-sanga <vulcan248@gmail.com>
2024-01-11 14:14:36 +0530

    feat: add neon db config and postgresql schema

---
+++ src/server/db/index.ts
@@ -0,0 +1,21 @@
+// import { Client } from "@planetscale/database";
+// import { drizzle } from "drizzle-orm/planetscale-serverless";
+
+// import { env } from "@/env";
+// import * as schema from "./schema";
+
+// export const db = drizzle(
+//   new Client({
+//     url: env.DATABASE_URL,
+//   }).connection(),
+//   { schema }
+// );
+
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+
+import { env } from "@/env";
+import * as schema from "./schema";
+
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+export const db = drizzle(pool, { schema });
