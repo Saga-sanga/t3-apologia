9bf216845802c4d4777fe4bd308be4bf0792e87b
Saga-sanga <vulcan248@gmail.com>
2024-01-22 21:45:25 +0530

    feat: add actions, filter and pagination to writer table

--- src/app/(writer)/dashboard/components/columns.tsx
+++ src/app/(writer)/dashboard/components/columns.tsx
@@ -1,7 +1,20 @@
 "use client";
 
-import { SelectZawhna, SelectUser } from "@/server/db/schema";
+import { Badge } from "@/components/ui/badge";
+import { SelectUser, SelectZawhna } from "@/server/db/schema";
 import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+import { format } from "timeago.js";
+import { ArrowUpDownIcon, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type ColumnDataType = SelectZawhna & {
   users: SelectUser | null;
@@ -10,18 +23,69 @@ type ColumnDataType = SelectZawhna & {
 export const columns: ColumnDef<ColumnDataType>[] = [
   {
     accessorKey: "status",
-    header: "Status",
+    header: () => <div className="">Status</div>,
+    cell: ({ row }) => {
+      const status = row.getValue("status");
+
+      return <Badge>{status as string}</Badge>;
+    },
   },
   {
     accessorKey: "content",
-    header: "Question",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Questions <ArrowUpDownIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
-  {
-    accessorFn: (row) => `${row.users?.email}`,
-    header: "Email",
-  },
+  // {
+  //   accessorFn: (row) => `${row.users?.name}`,
+  //   header: "Name",
+  // },
   {
     accessorKey: "createdAt",
-    header: "Date",
+    header: "Created",
+    // id: "who",
+    cell: ({ row }) => (
+      <div>
+        {format(row.getValue("createdAt"))} by{" "}
+        <Link className="underline-offset-4 hover:underline" href="/user">
+          {row.original.users?.name}
+        </Link>
+      </div>
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const obj = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(obj.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
