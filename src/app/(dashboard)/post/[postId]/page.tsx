5ddf28c678d3ee58add8e6da1bac9fac4aa19c42
Saga-sanga <vulcan248@gmail.com>
2024-02-02 12:49:46 +0530

    chore: add check for post page

--- src/app/(dashboard)/post/[postId]/page.tsx
+++ src/app/(dashboard)/post/[postId]/page.tsx
@@ -1,4 +1,5 @@
 import EditorTextParser from "@/components/editor-parser";
+import { getCurrentUser } from "@/server/auth";
 import { db } from "@/server/db";
 import { OutputData } from "@editorjs/editorjs";
 import { notFound } from "next/navigation";
@@ -14,12 +15,14 @@ export default async function PostPage({ params }: PostPageProps) {
     where: (posts, { eq }) => eq(posts.id, params.postId),
   });
 
-  if (!post) {
+  const user = await getCurrentUser();
+
+  if (!post || (post.state === "draft" && user?.role === "user")) {
     return notFound();
   }
 
   return (
-    <div className="prose prose-lg xl:prose-xl prose-stone dark:prose-invert mx-auto">
+    <div className="prose prose-lg xl:prose-xl prose-stone dark:prose-invert mx-auto w-full">
       <EditorTextParser data={post.content as OutputData} />
     </div>
   );
