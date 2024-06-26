92ce3990a55dfff95d3442e8da968cf73f0e5e1f
Saga-sanga <vulcan248@gmail.com>
2024-06-26 12:26:43 +0530

    design: made username page responsive

--- src/app/(dashboard)/[username]/page.tsx
+++ src/app/(dashboard)/[username]/page.tsx
@@ -28,13 +28,13 @@ export default async function Page({ params }: PageProps) {
     return notFound();
   }
   return (
-    <main className="mx-auto w-full max-w-screen-lg space-y-8 rounded-lg border px-16 py-12">
-      <div className="flex pb-2">
+    <main className="mx-auto w-full max-w-screen-lg space-y-8 rounded-lg lg:border px-6 sm:px-16 py-12">
+      <div className="flex flex-col sm:flex-row pb-2">
         <UserAvatar
           className="h-36 w-36"
           user={{ image: userData.image, name: userData.name }}
         />
-        <div className="my-6 flex flex-1 justify-between pl-10">
+        <div className="my-6 flex flex-col md:flex-row gap-5 flex-1 justify-between sm:pl-10">
           <div className="space-y-2 pr-5">
             <h1 className="text-3xl font-semibold">{userData.name}</h1>
             <p className="text-lg font-light tracking-wide text-foreground/80">
@@ -53,7 +53,7 @@ export default async function Page({ params }: PageProps) {
           )}
         </div>
       </div>
-      <div className="flex items-center justify-center space-x-10 rounded-lg border px-2 py-5 text-foreground/80">
+      <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 items-center justify-center space-x-10 rounded-lg border px-2 py-5 text-foreground/80">
         <span className="flex capitalize">
           <UserRound className="mr-2 h-5 w-5" />
           {userData.sex}
